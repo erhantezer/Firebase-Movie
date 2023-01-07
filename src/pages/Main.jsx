e4172a09ee71010ffff7bd9bbad90ac72f2cc3ef
Erhan <erhantezer68@gmail.com>
2023-01-07 22:46:53 +0300

    updated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,39 @@
+import { Box, Button, TextField } from "@mui/material";
 import React from "react";
-import MovieCard from "../components/MovieCard";
+// import MovieCard from "../components/MovieCard";
 
 const Main = () => {
   return (
-      <div>
-        <MovieCard/>
-      </div>
+      <>
+        <Box 
+          component="form" 
+          noValidate 
+          sx={{
+          marginTop:"6rem", 
+          display:"flex", 
+          justifyContent:"center", 
+          alignItems:"center" 
+        }}
+        >
+            <TextField
+              sx={{width:400}}
+              id="text"
+              label="Search Movie"
+              name="text"
+              autoComplete="text"
+              autoFocus
+              // onChange={(e) => setEmail(e.target.value)}
+            />
+          
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ ml: 2, padding:"0.8rem"}}
+            >
+              Search
+            </Button>
+          </Box>
+      </>
   );
 };
 
